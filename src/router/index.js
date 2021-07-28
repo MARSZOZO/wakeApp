ba774a653fc2d05a2e26d0535fde9240cf436836
Marsel Sultanov <marsel.sultanov.zozo@gmail.com>
2021-07-29 00:13:34 +0300

    delete other component vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Users from '../views/Users.vue'
 import UserDetail from '../views/UserDetail.vue'
 
 Vue.use(VueRouter)
@@ -8,8 +8,8 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Users',
+    component: Users
   },
   {
     path: '/users/:id',
